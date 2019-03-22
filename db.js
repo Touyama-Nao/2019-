b1139dec6c2387b8ad0f8e3f7707e5720ec137ed
滨边家的小美波 <3096237774@qq.com>
2019-03-22 15:14:58 +0800

    1

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 //导入数据库文件用mongoose连接方式
 var mongoose = require('mongoose');
-DB_URL = 'mongodb://localhost:27017/mytestdb';
+DB_URL = 'mongodb://localhost:27017/contactList';
 mongoose.connect(DB_URL);   //链接数据库
 
 mongoose.connection.on('connected',function(){
