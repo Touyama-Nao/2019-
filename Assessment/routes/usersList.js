cd540223aca4425afc1c56a3f4a6d4893380745b
滨边家的小美波 <3096237774@qq.com>
2019-03-22 14:06:58 +0800

    获取用户列表

--- Assessment/routes/usersList.js
+++ Assessment/routes/usersList.js
@@ -6,7 +6,11 @@ var bodyParser = require("body-parser");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log("I received a get request");
+	db.contactList.find(function (err, docs) {
+		console.log(docs);
+		res.json(docs);
+	})
 });
 
 module.exports = router;
